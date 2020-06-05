abcef88024d1ebf4b69835239c13381fff2e81a7
sanjeev- <sanjeev87@gmail.com>
2020-06-05 14:32:31 -0400

    Add new font

--- frontend/src/components/styledBaseComponents/ImpactTitle.js
+++ frontend/src/components/styledBaseComponents/ImpactTitle.js
@@ -2,9 +2,10 @@ import styled from "styled-components";
 
 // Create a Title component that'll render an <h1> tag with some styles
 const ImpactTitle = styled.h1`
-  font-size: 1.5em;
+  font-size: 3em;
   text-align: center;
   color: rgba(255, 46, 99, 1);
+  font-family: "Junicode";
 `;
 
 export default ImpactTitle;
--- frontend/src/components/styledBaseComponents/Title.js
+++ frontend/src/components/styledBaseComponents/Title.js
@@ -2,9 +2,10 @@ import styled from "styled-components";
 
 // Create a Title component that'll render an <h1> tag with some styles
 const Title = styled.h1`
-  font-size: 1.5em;
+  font-size: 3em;
   text-align: center;
   color: black;
+  font-family: "Junicode";
 `;
 
 export default Title;
