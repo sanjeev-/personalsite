cf0f8997ca0fc917577c08722ed7f1309f1562b5
sanjeev- <sanjeev87@gmail.com>
2020-06-20 14:38:17 -0400

    setup project base

---
+++ frontend/src/components/projects/UntitledZoraProject/ZoraProject.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Flex from "../../../styles/styles.js";
+import Title from "../../styledBaseComponents/Title";
+import SubHead from "../../styledBaseComponents/SubHead";
+import Text from "../../styledBaseComponents/Text";
+import Header from "../../Header.js";
+
+const ZoraProject = () => {
+  return <Flex>Zora</Flex>;
+};
+
+export default ZoraProject;
