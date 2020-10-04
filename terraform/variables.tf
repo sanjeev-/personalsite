edc0cbd94f7eba75780f8b3e1df956dd3eee0e87
sanjeev- <sanjeev87@gmail.com>
2020-10-04 15:25:15 -0400

    add tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "aws_region" {
+  type = string
+  description = "Administrator user name for virtual machine"
+}
+
+variable "aws_profile" {
+  type = string
+  description = "AWS profile to use"
+}
