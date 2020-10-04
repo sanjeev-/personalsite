ca1e449bce9c6d2d9530cbbfbe748b159b6096ba
sanjeev- <sanjeev87@gmail.com>
2020-10-04 15:28:03 -0400

    update gitignore and terraform vars;

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,14 @@
+aws_region = "us-east-1"
+aws_profile = "sanjeev_personal"
+availability_zones = ["us-east-1a", "us-east-1b"]
+health_check_path = "/ping/"
+ecs_cluster_name = "production"
+log_retention_in_days = 30
+ssh_pubkey_file = "~/.ssh/id_rsa.pub"
+amis = {us-east-1 = "ami-0667a9cc6a93f50fe"}
+instance_type = "t2.micro"
+docker_image_url_django = "501421885392.dkr.ecr.us-east-1.amazonaws.com/personal-site:latest"
+app_count = 2
+autoscale_min = 1
+autoscale_max = 10
+autoscale_desired = 4
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,5 @@
+# aws config
+
 variable "aws_region" {
   type = string
   description = "Administrator user name for virtual machine"
@@ -6,4 +8,70 @@ variable "aws_region" {
 variable "aws_profile" {
   type = string
   description = "AWS profile to use"
+}
+
+# networking
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR Block for Public Subnet 1"
+}
+variable "public_subnet_2_cidr" {
+  description = "CIDR Block for Public Subnet 2"
+}
+variable "private_subnet_1_cidr" {
+  description = "CIDR Block for Private Subnet 1"
+}
+variable "private_subnet_2_cidr" {
+  description = "CIDR Block for Private Subnet 2"
+}
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+}
+
+# load balancer
+
+variable "health_check_path" {
+  description = "Health check path for the default target group"
+}
+
+# logs
+
+variable "log_retention_in_days" {
+  description = "How many days we retain logs for"
+}
+
+# key pair
+
+variable "ssh_pubkey_file" {
+  description = "Path to an SSH public key"
+}
+
+# ecs
+
+variable "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+}
+variable "amis" {
+  description = "Which AMI to spawn."
+}
+variable "instance_type" {
+}
+variable "docker_image_url_django" {
+  description = "Docker image to run in the ECS cluster"
+}
+variable "app_count" {
+  description = "Number of Docker containers to run"
+}
+
+# auto scaling
+
+variable "autoscale_min" {
+  description = "Minimum autoscale (number of EC2)"
+}
+variable "autoscale_max" {
+  description = "Maximum autoscale (number of EC2)"
+}
+variable "autoscale_desired" {
+  description = "Desired autoscale (number of EC2)"
 }
