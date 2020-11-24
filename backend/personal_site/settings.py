9de7c91e8f2faacc83b246bd1f1c5cea338ce05d
sanjeev- <sanjeev87@gmail.com>
2020-11-24 00:24:42 -0500

    Update static files

--- backend/personal_site/settings.py
+++ backend/personal_site/settings.py
@@ -143,8 +143,3 @@ USE_TZ = True
 
 
 CORS_ORIGIN_ALLOW_ALL = True
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = "/static/"
